e363f5e7fc0e82e31c1c4b43a1de1749c920bc75
prdpta <pradiptaputramei01@gmail.com>
2024-03-19 18:31:18 +0700

    contoh 2

--- 037_LoopingFungsi2.cpp
+++ 037_LoopingFungsi2.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
 using namespace std;
 
-int main()
+int main(){
 
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    while (bilangan > 4)
+
+    {
+        cout << "Bilangan lebih dari 4" << endl;
+        bilangan = rand() % 10;
+        cout << "Bilangannya = " << bilangan << endl;
+    }
+
+    cout << "Bilangan terakhir = " << bilangan << endl;
+}
